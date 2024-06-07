2faff9a2ddfbdc6e350066dd17c20beffebf3f41
Alexis Hernández Kumul <getza.alget.15@gmail.com>
2024-06-07 14:45:14 -0500

    El primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FileUploadComponent } from './components/file-upload/file-upload.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule,FileUploadComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'saveFileAngular';
+  title = 'Guardar archivos con Angular y SpringBoot';
 }
